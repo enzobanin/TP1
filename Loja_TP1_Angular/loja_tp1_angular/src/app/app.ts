f82623442ae9ee7ab64e24a5128ea3bea4b07140
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-08-29 20:35:17 -0300

    Input,Output,Model - Aplicados no botao

--- Loja_TP1_Angular/loja_tp1_angular/src/app/app.ts
+++ Loja_TP1_Angular/loja_tp1_angular/src/app/app.ts
@@ -2,12 +2,21 @@ import { Component, signal } from '@angular/core';
 import { Header } from './core/header/header';
 import { Footer } from "./core/footer/footer";
 import { Banner } from './core/banner/banner';
+import { QuantidadeControle } from "./shared/quantidade-controle/quantidade-controle";
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer,Banner],
+  imports: [Header, Footer, Banner, QuantidadeControle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  sobre? : string;
+
+  qtd = signal(1);
+
+  receberSobre( texto : string){
+    this.sobre = texto;
+  }
 
 }
--- Loja_TP1_Angular/loja_tp1_angular/src/app/core/header/header.ts
+++ Loja_TP1_Angular/loja_tp1_angular/src/app/core/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-header', // para chamar em outro componente <app-header>
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
+  tituloLoja = input.required<string>(); //<app-header [tituloLoja] = "'Titulo'"></app-header>
 
+  textoSobre = output<string>();
+
+
+  enviarSobre(){ //:void
+    this.textoSobre.emit('Técnicas de Programação 1. Desenvolvido por Enzo.')
+  }
+
+  // exibirSobre(nome:string): void {
+  //   alert('Boa noite ' + nome);
+  // }
 }
