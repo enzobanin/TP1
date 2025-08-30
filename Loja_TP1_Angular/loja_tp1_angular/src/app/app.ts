9ac5ea33bc8e135b03ce4e2fc1bf6707e4345f42
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-08-29 22:17:56 -0300

    AdicionandoProdutos,quantidades,botoes,etc.

--- Loja_TP1_Angular/loja_tp1_angular/src/app/app.ts
+++ Loja_TP1_Angular/loja_tp1_angular/src/app/app.ts
@@ -3,13 +3,19 @@ import { Header } from './core/header/header';
 import { Footer } from "./core/footer/footer";
 import { Banner } from './core/banner/banner';
 import { QuantidadeControle } from "./shared/quantidade-controle/quantidade-controle";
+import { Produto } from './model/produto';
+import { CardProduto } from "./features/produtos/card-produto/card-produto";
+import { ListaProdutos } from "./features/produtos/lista-produtos/lista-produtos";
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Banner, QuantidadeControle],
+  imports: [Header, Footer,ListaProdutos], //Banner, QuantidadeControle
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  
+
 
   sobre? : string;
 
---
+++ Loja_TP1_Angular/loja_tp1_angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Produto } from '../../../model/produto';
+import { CardProduto } from "../card-produto/card-produto";
+
+@Component({
+  selector: 'lista-produtos',
+  imports: [CardProduto],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css'
+})
+export class ListaProdutos {
+  produtos: Produto[] = [
+    {
+      id: 1,
+      nome: 'Produto 1',
+      descricao: 'Desc. Prod 1',
+      preco: 179.90,
+      imageURL: 'images/brasil2022.png',
+      promo:true
+    },
+    {
+      id: 2,
+      nome: 'Produto 2',
+      descricao: 'Desc. Prod 2',
+      preco: 101,
+      imageURL: 'images/brasil2024gk.png'
+    },
+    {
+      id: 3,
+      nome: 'Produto 3',
+      descricao: 'Desc. Prod 3',
+      preco: 601.99
+    }
+  ]
+
+  onAddProduct(produto:{id:number, quantity:number}){
+    alert(`Produto ${produto.id}, ${produto.quantity} unidades`);
+  }
+
+}
+
