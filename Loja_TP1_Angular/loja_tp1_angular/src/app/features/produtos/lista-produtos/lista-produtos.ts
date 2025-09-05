888404caf8e823ec2deb59d8820938f4cbd67e1d
enzobanin <baninenzo@gmail.com>
2025-09-05 20:36:25 -0300

    AdicionandoSelos

--- Loja_TP1_Angular/loja_tp1_angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ Loja_TP1_Angular/loja_tp1_angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { CardProduto } from "../card-produto/card-produto";
 
@@ -16,26 +16,41 @@ export class ListaProdutos {
       descricao: 'Desc. Prod 1',
       preco: 179.90,
       imageURL: 'images/brasil2022.png',
-      promo:true
+      promo:true,
+      estado: 'usado'
     },
     {
       id: 2,
       nome: 'Produto 2',
       descricao: 'Desc. Prod 2',
       preco: 101,
-      imageURL: 'images/brasil2024gk.png'
+      imageURL: 'images/brasil2024gk.png',
+      estado: 'novo'
     },
     {
       id: 3,
       nome: 'Produto 3',
       descricao: 'Desc. Prod 3',
       preco: 601.99,
-      imageURL: 'images/camisa2selecaoAzul.png'
+      imageURL: 'images/camisa2selecaoAzul.png',
+      estado: 'esgotado'
     }
   ]
 
+  apenasPromo = signal(false);
+
+  prodExibidos = computed(() => this.apenasPromo() ? this.produtos.filter(p => p.promo): this.produtos);
+
+  alternarPromo(){
+    this.apenasPromo.update(p => !p);
+  }
+
   onAddProduct(produto:{id:number, quantity:number}){
     alert(`Produto ${produto.id}, ${produto.quantity} unidades`);
+  }
+
+  onViewProduct(id:number){
+    alert(`Id do produto ${id}`);
   }
 
 }
